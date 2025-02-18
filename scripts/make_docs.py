1066e4fea84d87280fc4d309d3c08a0968ba5bb6
Zijian Zhang <doomspec@outlook.com>
2025-02-17 19:34:57 -0500

    add guidance

--- scripts/make_docs.py
+++ scripts/make_docs.py
@@ -9,5 +9,12 @@ output_path = sys.argv[1]
 G = generate_network_from_module(chem_net)
 description = """
 Modify the network on <a href="https://github.com/aspuru-guzik-group/ReprNet">Github</a>
+<br/>
+<b>How to read the graph?</b> 
+<br/>
+Nodes: Green diamond: A transform; Red box: A representation; 
+<br/>
+Edges: Red line: Input edge; Green line: Output edge; Blue dashed line: Composer edge; Yellow line: Abstraction edge.
+<br/>
 """
 display_network(G, show_browser=False, output_path=output_path, title="Chemistry Representation Network", description=description)
